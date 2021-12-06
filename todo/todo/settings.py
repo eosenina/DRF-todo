526398ed9236d0cda68ebccc21b621aae971f545
Екатерина Осенина <ekaterinaosenina@macbook.osenin.me>
2021-12-06 20:11:30 +0300

    homework6 tasks 2 and 3

--- todo/todo/settings.py
+++ todo/todo/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'corsheaders',
     'project',
     'django_filters',
+    'rest_framework.authtoken'
 ]
 
 MIDDLEWARE = [
@@ -136,7 +137,14 @@ REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': ['rest_framework.renderers.JSONRenderer'],
     # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     # 'PAGE_SIZE': 100,
-    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    ]
 }
 
 if DEBUG:
--- todo/todo_users/views.py
+++ todo/todo_users/views.py
@@ -1,3 +1,4 @@
+from rest_framework.permissions import IsAdminUser
 from rest_framework.viewsets import ModelViewSet
 from .models import User
 from .serializers import UserModelSerializer
@@ -12,3 +13,4 @@ from rest_framework.viewsets import GenericViewSet
 class UserCustomViewSet(ListModelMixin, RetrieveModelMixin, UpdateModelMixin, GenericViewSet):
     queryset = User.objects.all()
     serializer_class = UserModelSerializer
+    # permission_classes = [IsAdminUser]
